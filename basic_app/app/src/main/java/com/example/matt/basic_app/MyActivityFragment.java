70ac94f082dffe26bfca6df4b402a18808ff1b2f
mdeyell <mdeyell@ucsc.edu>
2016-06-17 15:22:32 -0700

    got tutorial setup

---
+++ basic_app/app/src/main/java/com/example/matt/basic_app/MyActivityFragment.java
@@ -0,0 +1,22 @@
+package com.example.matt.basic_app;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MyActivityFragment extends Fragment {
+
+    public MyActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_my, container, false);
+    }
+}
